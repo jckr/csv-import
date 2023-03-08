9dce92e07216c6b39433faa2395dc4afb858a6d8
Jerome Cukier <jeromecukier+github@gmail.com>
2023-03-08 12:42:40 -0800

    types setup

--- src/lib/csv-import.ts
+++ src/lib/csv-import.ts
@@ -1,1 +1,19 @@
-export const csvImport = () => {}
+type Value = string | number | null;
+type Header = {
+  name: string;
+  nullable: boolean;
+  type: 'string' | 'number' | 'null' | 'unknown';
+}
+
+type csv = {
+  data: Value[][];
+  headers: Header[];
+}
+
+export const csvImport = (raw: string): csv => {
+
+  return {
+    data: [] as Value[][],
+    headers: [] as Header[],
+  };
+}
